b5ee925c90fa89fc650bdeaebac5ce9d329ae9ca
Guy Rozen <HolyEggplant@gmail.com>
2014-10-14 23:24:10 +0300

    This is now a proper Cabal package

--- main.hs
+++ main.hs
@@ -1,11 +1,16 @@
 import System.Environment
 import System.IO
 import Control.Concurrent
-import Control.Monad
+import Control.Monad.Loops
 import qualified Board as Board
 
 binaryToCell '1' = Board.Full
 binaryToCell '0' = Board.Empty
+
+tickBoard :: Board -> IO Board
+tickBoard board =
+  let ticked = tick board
+  in  threadDelay 1000000 >> print ticked >> return ticked
 
 main = do
   boardFile:_ <- getArgs
@@ -14,7 +19,9 @@ main = do
   board <- return (Board.fromRowList $ map (\row -> map (\char -> binaryToCell char) row) (lines contents))
   print board
 
-  forever $ do
-    threadDelay 10000
-    board <- return (Board.tick board)
+  iterateM_ $ do
+    threadDelay 1000000
+    --board <- return (Board.tick board)
+    iterateM_ $
     print board
+    return board

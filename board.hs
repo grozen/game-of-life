e2abea12439a18f7792c47bd437007b0522d48ad
Guy Rozen <HolyEggplant@gmail.com>
2014-10-14 10:52:54 +0300

    This is getting out of hand. Changing approach.

--- board.hs
+++ board.hs
@@ -14,12 +14,23 @@ data Board = Board [[Cell]]
 instance Show Board where
   show (Board []) = "Null board"
   show (Board [cells]) = concat $ map show $ map fst cells
-  show (Board (cells:lines)) = show (Board [cells]) ++ "\n" ++ show (Board lines)
+  show (Board (cells:rows)) = show (Board [cells]) ++ "\n" ++ show (Board rows)
 
 resetNeighbours :: Board -> Board
 resetNeighbours (Board rows) = Board $ map resetRow rows
   where resetRow [] = []
         resetRow cells = map (\(contents, neighbours) -> (contents, 0)) cells
 
---calculateNeighbours :: Board -> Board
+calculateNeighbours :: Board -> Board
+calculateNeighbours (Board rows) = Board $ pushNeighbours $ pullNeighbours rows
+  where pushNeighbours [] = []
+        pushNeighbours [[]] = [[]]
+        pushNeighbours [cells] = [pushRow cells]
+        pushNeighbours [cells:nextCells:rows] = (pushRow cells):(pushNeighbours (pushNextRow cells nextCells):rows)
+        pushRow [] = []
+        pushRow [_] = [_]
+        pushRow first@(Full, _):(content, neighbours):cells = first:(pushRow (content, neighbours + 1):cells)
+        updateNextRow [] nextCells = nextCells
+        -- Maybe some take 2 drop 2 action here?
+        -- updateNextRow [(Full, _)] nextCells = nextCells
 
